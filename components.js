716133a2946639cb1a552538442b793f3bd7f1b7
Holayn <kwwong1597@gmail.com>
2021-01-02 02:05:46 -0500

    feat(xpskills): implement

--- components.js
+++ components.js
@@ -2,6 +2,7 @@ import Vue from 'https://cdn.jsdelivr.net/npm/vue@2.6.12/dist/vue.esm.browser.js
 
 import {makeRectangle} from './rectangle.js';
 import {navigate} from './utils.js';
+import {experiences} from './experiences.js';
 
 import {defaultContent} from './about/default.js';
 import {education} from './about/education.js';
@@ -173,14 +174,55 @@ export const Projects = Vue.component('projects', {
 });
 
 export const Blog = Vue.component('blog', {
-  template: '#xpskills',
+  template: '#blog',
   methods: {
     navigate,
   },
 });
 
+Vue.component('experience-item', {
+  template: /*html*/`
+  <div class="grid grid-cols-8 p-6 gap-4">
+    <div class="col-span-2 px-6">
+      <img :src="img"/>
+    </div>
+    <div class="grid col-span-6 bright-text gap-4" style="grid-template-rows: auto auto 1fr;">
+      <div class="italic">
+        <div class="text-2xl">{{experience.name}}</div>
+        <div class="text-xl">{{experience.date}}</div>
+        <div class="text-xl">{{experience.role}}</div>
+      </div>
+      <div class="text-xl">Stack: {{experience.stack}}</div>
+      <div>
+        <div v-for="detail in experience.details" :key="detail.id" class="text-xl py-2">
+          <div>{{detail.heading}}</div>
+          <ul class="list-disc pl-6">
+            <li v-for="content in detail.content">
+              {{content}}
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+  `,
+  props: ['img', 'item'],
+  data: () => {
+    return {
+      experiences,
+    };
+  },
+  methods: {
+  },
+  computed: {
+    experience: function() {
+      return this.experiences[this.item];
+    }
+  }
+});
+
 export const XpSkills = Vue.component('xpskills', {
-  template: '#blog',
+  template: '#xpskills',
   methods: {
     navigate,
   },
---
+++ experiences.js
@@ -0,0 +1,83 @@
+export const experiences = {
+  factset: {
+    stack: 'TypeScript/JavaScript, Vue, HTML/CSS, Node.js, FactSet Query Language, Heroku, Jenkins',
+    name: 'FactSet Research Systems Inc.',
+    date: 'Feb 2019 - Current',
+    role: 'Software Engineer II',
+    details: [
+      {
+        heading: 'Data APIs',
+        content: [
+          'Developed service to serve APIs that returned web app data for clients and internal applications, with a focus on performance, scalability, and format of data best suited for displaying data',
+        ],
+      },
+      {
+        heading: 'Guide',
+        content: [
+          'Developed web app to display concise financial data via tiles with a heavy focus on performance receiving thousands of hits daily',
+          'Lead engineering initiative in developing a generic configurable solution to creating tiles',
+          'Lead research initiative in discovering a suitable printing solution',
+          'Introduced various performance enhancements to the app to greatly improve user experience',
+        ],
+      },
+      {
+        heading: 'Navigator',
+        content: [
+          'Maintained and contributed features/bug fixes/refactors to critical infrastructure web app receiving 100k+ hits daily',
+          'Reviewed/accepted other internal teams\' contributions',
+        ],
+      },
+      {
+        heading: 'ESG, Signals Gallery/Ribbon',
+        content: [
+          'Developed various web apps to display various forms of financial data',
+        ],
+      },
+      {
+        heading: 'Misc',
+        content: [
+          'Collaborated closely on various projects with other engineers and product developers in an agile setting',
+          'Driven to be independent as a team and continuously find ways to improve',
+          'Provided in-depth code reviews on a daily basis',
+          'Maintained codebases and provided support for internal/client requests',
+          'Heavy emphasis on shift-left testing, and created/supported thorough documentation',
+          'Contributed to core codebases (web components, web app framework)',
+          'Mentored team new hires, ran Chrome developer tools training lecture',
+          'Delivered presentations on engineering topics',
+        ],
+      },
+    ],
+  },
+  factsetIntern: {
+    stack: 'AngularJS, Node.js, TypeScript, Python',
+    name: 'FactSet Research Systems Inc.',
+    date: 'May 2018 - Aug 2018',
+    role: 'Software Engineer Intern',
+    details: [
+      {
+        heading: 'Executing R Scripts in an Existing Web App',
+        content: [
+          'Developed/integrated full stack app into FactSet’s Alpha Testing and across pre-existing modules to increase reporting capability/flexibility and streamline client workflow',
+          'Presented to sales/directors, worked closely with engineers/product developers in agile development',
+        ],
+      },
+    ],
+  },
+  ibm: {
+    stack: 'AngularJS, Java, Node.js, Python, Karma, Jenkins',
+    name: 'IBM',
+    date: 'May 2018 - Dec 2018',
+    role: 'Software Engineer Intern',
+    details: [
+      {
+        heading: 'Developed tools to facilitate continuous delivery of z/OS documentation',
+      },
+      {
+        heading: 'Created Angular web app for IBM’s service request system’s move to cloud',
+      },
+      {
+        heading: 'Integrated IBM Watson & other services into Pepper robot',
+      },
+    ],
+  }
+};
